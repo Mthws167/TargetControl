9ee2fc5dcffaec4cc042d2d5e5171ef0314ff5e6
Matheus Henrique de Oliveria <mthws.henrique@hotmail.com>
2022-08-04 23:18:58 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,30 @@
-
-import 'package:countpeople/utils.dart';
-import 'package:countpeople/screens/authPage.dart';
+import 'package:countpeople/screens/screen-login/login.dart';
 import 'package:countpeople/screens/screen-counter/counter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-Future main() async {
+Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Target Control',
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return const Center(child: Text('Erro Inesperado!'));
-          } else if (snapshot.hasData) {
-            return const Counter();
-          } else {
-            return const AuthPage();
-          }
-        },
-      ));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Target Control',
+    home: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context,snapshot){
+        if(snapshot.hasData){
+          return Counter();
+        }else{
+          return Login();
+        }
+      },
+    )
+  ));
 }
 
 class TargetControl extends StatefulWidget {
@@ -41,14 +34,10 @@ class TargetControl extends StatefulWidget {
   State<TargetControl> createState() => _TargetControlState();
 }
 
-final navigatorKey = GlobalKey<NavigatorState>();
-
 class _TargetControlState extends State<TargetControl> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      scaffoldMessengerKey: Utils.messengerKey,
-      navigatorKey: navigatorKey,
-    );
+      return MaterialApp(
+      );
   }
 }
