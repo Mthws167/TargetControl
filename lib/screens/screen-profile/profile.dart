557a01d0e27ffe7f04fcc4733e930c7fc9f45bfb
Mthws167 <mthws.henrique@hotmail.com>
2022-10-04 13:57:00 -0300

    aditando profile

--- lib/screens/screen-profile/profile.dart
+++ lib/screens/screen-profile/profile.dart
@@ -12,31 +12,22 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title: Text(user.displayName!),
-        backgroundColor: Colors.deepPurpleAccent,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(25),
+        appBar: AppBar(
+          title: Text(user.displayName!),
+          backgroundColor: Colors.deepPurpleAccent,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(25),
+            ),
           ),
         ),
-        // actions: [
-        //   IconButton(
-        //     alignment: Alignment.center,
-        //     icon: const Icon(Icons.exit_to_app_outlined),
-        //     onPressed: () {
-        //       FirebaseAuth.instance.signOut();
-        //     },
-        //   )
-        // ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const <Widget>[
-          ProfileData(),
-        ],
-      ),
-    );
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            ProfileData(),
+          ],
+        ),
+      );
 }
 
 class ProfileData extends StatefulWidget {
@@ -49,40 +40,57 @@ class ProfileData extends StatefulWidget {
 }
 
 class ProgressState extends State<ProfileData> {
+  final user = FirebaseAuth.instance.currentUser!;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        SizedBox(
-          width: 250,
-          height: 35,
-          child: ElevatedButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  side: const BorderSide(color: Colors.deepPurpleAccent),
+    return Center(
+      child: Column(
+        children: <Widget>[
+          SizedBox(),
+          const SizedBox(
+            height: 40,
+          ),
+          SizedBox(
+            child: Text(
+              (user.email!),
+              style: const TextStyle(
+                  fontSize: 18, decoration: TextDecoration.overline),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          SizedBox(
+            width: 250,
+            height: 35,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    side: const BorderSide(color: Colors.deepPurpleAccent),
+                  ),
                 ),
               ),
-            ),
-            onPressed: () async {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => PDFPage(),
+              onPressed: () async {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => PDFPage(),
+                  ),
+                );
+              },
+              child: const Text(
+                'Relatório em PDF? Clique Aqui',
+                style: TextStyle(
+                  color: Colors.deepPurpleAccent,
                 ),
-              );
-            },
-            child: const Text(
-              'Relatório em PDF? Clique Aqui',
-              style: TextStyle(
-                color: Colors.deepPurpleAccent,
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
