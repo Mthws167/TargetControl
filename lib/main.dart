e95cae6a8fc1d862bd223078c05f586495aeb105
Mthws167 <mthws.henrique@hotmail.com>
2022-10-26 18:02:43 -0300

    animação de tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:countpeople/screens/authPage.dart';
 import 'package:countpeople/screens/screen-counter/counter.dart';
 import 'package:countpeople/utils/utils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:lottie/lottie.dart';
+import 'package:page_transition/page_transition.dart';
 import 'firebase_options.dart';
+
+
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +17,7 @@ Future main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
+  runApp(const SplashScreen());
 }
 
 final navigatorKey = GlobalKey<NavigatorState>();
@@ -25,9 +30,28 @@ class MyApp extends StatelessWidget {
         scaffoldMessengerKey: Utils.messengerKey,
         navigatorKey: navigatorKey,
         debugShowCheckedModeBanner: false,
-        home: TargetControl(),
+        home:TargetControl(),
       );
 }
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+        splash: Lottie.asset('assets/splash/112042-target-commercial.json'),
+        backgroundColor: Colors.black,
+        nextScreen: const MyApp(),
+      splashIconSize: 250,
+      duration: 3000,
+      splashTransition: SplashTransition.sizeTransition,
+      pageTransitionType: PageTransitionType.leftToRightWithFade,
+      animationDuration: const Duration(seconds: 1),
+    );
+  }
+}
+
 
 class TargetControl extends StatelessWidget {
   @override
